40ca24247581224b10c46338091592323d49d203
exyi <exyi@outlook.cz>
2018-11-18 09:54:44 +0000

    Added basic CLI compiler

--- src/TrainedMonkey.CLI/Program.cs
+++ src/TrainedMonkey.CLI/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using ICSharpCode.Decompiler.TypeSystem;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using TrainedMonkey.CSharpGen;
+using IO = System.IO;
 
 namespace TrainedMonkey.CLI
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.In.ReadToEnd();
+
+            var schema = TrainedMonkey.GraphqlLoader.GraphqlLoader.LoadFromGraphQL(new [] { ("stdin.gql", new Lazy<string>(input)) });
+            var backend = new CSharpBackend();
+            var settings = new EmitSettings("GeneratedProject.ModelNamespace",
+                ImmutableDictionary.CreateRange<string, FullTypeName>(new Dictionary<string,FullTypeName>{
+                    ["Int"] = new FullTypeName("System.Int32"),
+                    ["String"] = new FullTypeName("System.String"),
+                    ["ID"] = new FullTypeName("System.Guid"),
+                    ["Float"] = new FullTypeName("System.Double"),
+                    ["Boolean"] = new FullTypeName("System.Boolean"),
+                }));
+            var result = backend.Build(schema, settings);
+            Console.WriteLine(result);
+
+            // Emit(args[0], args[1]).Wait();
         }
+        // public static async Task Emit(string projectPath, string outPath)
+        // {
+        //     // new Microsoft.CodeAnalysis.MSBuild.MSBuildProjectLoader().LoadProjectInfoAsync()
+        //     var w = MSBuildWorkspace.Create();
+        //     var s = await w.OpenSolutionAsync(projectPath);
+        //     var p = s.Projects.First(p_ => p_.Name.Contains("CLI"));
+        //     var c = await p.GetCompilationAsync();
+
+        //     using (var dllFile = IO.File.Create(IO.Path.Combine(outPath, c.AssemblyName + ".dll")))
+        //     using (var pdbFile = IO.File.Create(IO.Path.Combine(outPath, c.AssemblyName + ".dll.pdb")))
+        //     {
+        //         var result = c.Emit(dllFile, pdbFile);
+        //         if (!result.Success)
+        //             Console.WriteLine("Some error:");
+        //         foreach(var d in result.Diagnostics)
+        //             Console.WriteLine(d);
+        //     }
+        // }
     }
 }
